fc3d17b1d24ecadb94b85806bee24b71db4e1a12
kseniiavi <k8i8v8@gmail.com>
2024-03-24 11:14:07 -0400

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+    if year % 4 == 0 {
+        return true;
+    }
+    if year % 100 == 0 && year % 400 == 0 {
+        return true;
+    } else {
+        return false;
+    }
+
 }
